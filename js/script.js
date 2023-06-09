cecf77d2a71bf4bf0ef2190d4fec9ff7c5f25279
Tommicavo <tommicava@icloud.com>
2023-06-09 15:08:52 +0200

    final

--- js/script.js
+++ js/script.js
@@ -7,23 +7,44 @@ const result = document.getElementById("result");
 const countdownField = document.getElementById("countdownField");
 const randomNumContainer = document.getElementById("randomNumContainer");
 const answerContainer = document.getElementById("answerContainer");
+const score = document.getElementById("score");
+const guessList = document.getElementById("guessList");
+
+const send = document.getElementById("send");
+
 const numToDisplay = 5;
 const sDelay = 5;
 const msDelay = sDelay * 1000;
 
 const generateRandomNums = (howmany, questContainer, answContainer) => {
+    const numList = [];
     let randomNums = "";
     let anwerInput = "";
     for (let i = 0; i < howmany; i++){
         const rand = Math.floor(Math.random() * 100) + 1;
+        numList.push(rand);
         randomNums += `<span id="ranNum_${i + 1}">${rand}</span>`;
         anwerInput += `<input type="text" id="ans_${i + 1}">`;
     }
     questContainer.innerHTML = randomNums;
     answContainer.innerHTML = anwerInput;
+    return numList
 };
 
-generateRandomNums(numToDisplay, randomNumContainer, answerContainer);
+const showResult = (pcList, playerList) => {
+    let currentScore = 0;
+    let guessList = "";
+    for (let i = 0; i < playerList.length; i++){
+        if (pcList.includes(playerList[i])){
+            currentScore += 1;
+            guessList += `${playerList[i]}, `;
+        }
+    }
+    return [currentScore, guessList.substring(0, guessList.length - 2)]
+};
+
+const randNumList = generateRandomNums(numToDisplay, randomNumContainer, answerContainer);
+console.log(randNumList);
 
 let countdownValue = sDelay;
 countdownField.innerText = countdownValue;
@@ -38,6 +59,25 @@ const countdown = setInterval(() => {
         answer.classList.remove("d-none");
     }
 }, 1000);
+
+
+send.addEventListener("click", () => {
+    const ansList = [];
+    for (let i = 0; i < numToDisplay; i++){
+        const ansNum = document.getElementById(`ans_${i + 1}`).value;
+        if (!isNaN(parseInt(ansNum))) ansList.push(parseInt(ansNum));
+        
+    }
+    console.log(ansList);
+    answer.classList.add("d-none");
+    result.classList.remove("d-none");
+    const resultArray = showResult(randNumList, ansList);
+    score.innerText = resultArray[0];
+    guessList.innerText = resultArray[1];
+});
+
+
+
 
 
 
